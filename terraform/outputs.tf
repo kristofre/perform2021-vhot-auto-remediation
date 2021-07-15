6d3de965ff399ae7f2372a249bc3c70f2092df05
luisescobar89 <luis.scobr@gmail.com>
2021-07-15 11:58:19 -0500

    update outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,3 +8,7 @@ output "users" {
     user.email => google_compute_instance.ubuntu-vm[index].network_interface[0].access_config[0].nat_ip
   }
 }
+
+output "ubuntu_ip" {
+  value = "connect using: ssh -i ${path.module}/key ${var.gce_username}@ip_address"
+}
